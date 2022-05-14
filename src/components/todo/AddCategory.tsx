11b3eac071b8af7241a5d7a68d2cf68eb36e5649
furusetomoya <tomo.fwork18@gmail.com>
2022-05-15 01:13:40 +0900

    カテゴリーの追加

--- src/components/todo/AddCategory.tsx
+++ src/components/todo/AddCategory.tsx
@@ -15,39 +15,44 @@ type Props = {
 
 export default function AddCategory(props: Props) {
   const [title, setTitle] = useState<string>('')
-  const [groupId, setGroupId] = useState<string>('')
   const { user, group } = props
 
-  const createCategory = async () => {
+  const addCategory = async () => {
+    console.log('addCategory')
     const familyGroupCollectionRef = collection(db, 'familyGroup', group!.id, 'todoCategory')
+    const docRef = await addDoc(familyGroupCollectionRef, { id: '', name: title, })
+    console.log(docRef.id)
+    await updateDoc(doc(db, 'familyGroup', group!.id, 'todoCategory', docRef.id), {
+      id: docRef.id,
+    })
   }
 
-  const addUserInGroup = async (id: string, groupId: string) => {
-    const userDoc = doc(db, 'users', id)
-    const newFields = { groupId: groupId }
-    await updateDoc(userDoc, newFields)
-  }
+  // const addUserInGroup = async (id: string, groupId: string) => {
+  //   const userDoc = doc(db, 'users', id)
+  //   const newFields = { groupId: groupId }
+  //   await updateDoc(userDoc, newFields)
+  // }
 
   const handleChangeTitle = (e: any) => {
     setTitle(e.target.value)
   }
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault()
-    createCategory()
-  }
+  // const handleSubmit = (e: any) => {
+  //   e.preventDefault()
+  //   createCategory()
+  // }
 
   return (
     <Container component='main' maxWidth='xs'>
-      <Box component='form' noValidate onSubmit={handleSubmit}>
+      <Box>
         <TextField
           id='outlined-basic'
           label='Outlined'
           variant='outlined'
           onChange={handleChangeTitle}
         />
-        <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
-          Create Category
+        <Button onClick={addCategory} fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+          Add Category
         </Button>
       </Box>
     </Container>
